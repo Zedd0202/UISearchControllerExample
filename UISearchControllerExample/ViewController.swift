a60db8a650fdc2fcbc0187d0609abd5f79492dff
Zedd0202 <ch41150@hanmail.net>
2021-01-10 22:30:27 +0900

    UISearchController 예제 구현 완료

--- UISearchControllerExample/ViewController.swift
+++ UISearchControllerExample/ViewController.swift
@@ -9,50 +9,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     var arr = ["Zedd", "Alan Walker", "David Guetta", "Avicii", "Marshmello", "Steve Aoki", "R3HAB", "Armin van Buuren", "Skrillex", "Illenium", "The Chainsmokers", "Don Diablo", "Afrojack", "Tiesto", "KSHMR", "DJ Snake", "Kygo", "Galantis", "Major Lazer", "Vicetone"
     ]
     
+    var filteredArr: [String] = []
+    
+    var isFiltering: Bool {
+        let searchController = self.navigationItem.searchController
+        let isActive = searchController?.isActive ?? false
+        let isSearchBarHasText = searchController?.searchBar.text?.isEmpty == false
+        return isActive && isSearchBarHasText
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let resultViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "ResultViewController") as! ResultViewController
+        self.setupSearchController()
+        self.setupTableView()
+    }
+    
+    func setupSearchController() {
         let searchController = UISearchController(searchResultsController: nil)
         searchController.searchBar.placeholder = "Search Dj"
-        searchController.searchBar.scopeButtonTitles = [
-            "안녕", "하세요", "Zedd", "입니다"
-        ]
-        searchController.searchBar.scopeBarBackgroundImage = UIImage(systemName: "heart")
-        
-        searchController.searchBar.showsScopeBar = true
-        searchController.automaticallyShowsScopeBar = true
-
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchResultsUpdater = self
         self.navigationItem.searchController = searchController
-
         self.navigationItem.title = "Search"
-        
-        //self.navigationController?.navigationBar.prefersLargeTitles = true
-        
         self.navigationItem.hidesSearchBarWhenScrolling = false
-        
-        
-//        self.tableView.delegate = self
-//        self.tableView.dataSource = self
-        
     }
-
-
+    
+    func setupTableView() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.arr.count
+        return self.isFiltering ? self.filteredArr.count : self.arr.count
     }
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = self.arr[indexPath.row]
+        if self.isFiltering {
+            cell.textLabel?.text = self.filteredArr[indexPath.row]
+        } else {
+            cell.textLabel?.text = self.arr[indexPath.row]
+        }
         return cell
     }
-    
 }
 
+extension ViewController: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let text = searchController.searchBar.text?.lowercased() else { return }
+        self.filteredArr = self.arr.filter { $0.localizedCaseInsensitiveContains(text) }
+        
+        self.tableView.reloadData()
+    }
+}
